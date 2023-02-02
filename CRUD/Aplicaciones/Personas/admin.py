1e5537bcfdb9b973b6df37df6642ad63c9afda84
Wan021 <114534772+Wan021@users.noreply.github.com>
2023-02-01 22:36:20 -0600

    Solución de ordenamiento, agregado lista de seleccion en pais y genero

--- CRUD/Aplicaciones/Personas/admin.py
+++ CRUD/Aplicaciones/Personas/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Persona
+from .models import Persona, Nacionalidades
 # Register your models here.
 
 admin.site.register(Persona)
+admin.site.register(Nacionalidades)
